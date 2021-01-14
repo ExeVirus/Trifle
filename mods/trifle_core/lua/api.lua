5ffd28cdb0d157db5705ff9e1f049a4d5a8f8872
ExeVirus <44562154+ExeVirus@users.noreply.github.com>
2021-01-14 15:40:02 -0500

    Complete alpha version of api.lua

--- mods/trifle_core/lua/api.lua
+++ mods/trifle_core/lua/api.lua
@@ -1,5 +1,11 @@
 -----External Focused API-----
 
+-------------------------------------------------------------
+--
+-- trifle.add_set(name_of_set, icon_texture_name)
+--
+-- Pretty straight forward, right?
+-------------------------------------------------------------
 function trifle.add_set(name_of_set, icon_texture_name)
 	if not trifle.levels.name_of_set then
 		trifle.levels[name_of_set] = {}
@@ -11,13 +17,43 @@ function trifle.add_set(name_of_set, icon_texture_name)
 	end
 end
 
---level_file is a table containing a .map among callbacks and whatnot
-function trifle.add_level(name_of_set, name_of_level, level_number, level_file)
+-------------------------------------------------------------
+--
+-- trifle.add_level(name_of_set, name_of_level, level_number, level_file)
+--
+-- see core.lua to learn about a level_def
+-------------------------------------------------------------
+function trifle.add_level(name_of_set, name_of_level, level_number, level_def)
 	if not trifle.levels[name_of_set][level_number] then
 		trifle.levels[name_of_set][level_number] = {}
 		trifle.levels[name_of_set][level_number].name = name_of_level
-		trifle.levels[name_of_set][level_number].level = table.copy(level_file)
+		trifle.levels[name_of_set][level_number].level = table.copy(level_def)
 	else
-		minetest.log("Unable to register new "..name_of_set.." level number "..level_number..". It already exists!")
+		minetest.log("error", "Unable to register new "..name_of_set.." level number "..level_number..".\nIt already exists!")
 	end
+end
+
+-------------------------------------------------------------
+--
+-- trifle.add_action(name,func)
+--
+-- func must be a function(action_def)
+--
+-- where action_def = {
+--              action = string, --can safely ignore this, handled by trifle.core
+--              delay = number,  --can safely ignore this, handled by trifle.core
+--              round = number,  --can safely ignore this, handled by trifle.core
+--
+--              --and the rest of the table can be whatever you want it to have to make
+--                your action work. for example:
+--              custom1 = "fun",
+--              num = 5,
+--
+--              --and so on
+--          }
+-------------------------------------------------------------
+function trifle.add_action(name,func)
+    if type(name) ~= "string" then minetest.log("error", "trifle_core:Invalid string entered for action registration") end
+    if trifle.actions[name] ~= nil then minetest.log("error", "trifle_core:"..name.." is already registered as an action") end
+    trifle.actions[name] = func
 end
